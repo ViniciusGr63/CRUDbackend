874e6852699fd1fb5930a89b269705fb36844f07
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2024-09-03 19:03:43 +0000

    back

---
+++ crud/src/main/java/com/example/CRUDbackend/CruDbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDbackendApplication.class, args);
+	}
+
+}
---
+++ crud/src/test/java/com/example/CRUDbackend/CruDbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
